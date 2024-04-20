1b04498f6d3163229ffc99294b692490363bf21a
fateme.r <fatemeh.r3366@gmail.com>
2024-04-20 15:24:36 +0330

    Fix peer depencies and test data

--- packages/permit-check/tests/explorer.spec.ts
+++ packages/permit-check/tests/explorer.spec.ts
@@ -2,6 +2,7 @@ import ergoExplorerClientFactory from '@rosen-clients/ergo-explorer';
 import { describe, expect, it, vitest } from 'vitest';
 
 import { TestExplorerPermitHealthCheck } from './testExplorer';
+import { explorerUnspentBoxesByAddress } from './testData';
 
 vitest.mock('@rosen-clients/ergo-explorer');
 
@@ -23,66 +24,7 @@ describe('ExplorerPermitAssetHealthCheckParam', () => {
       vitest.mocked(ergoExplorerClientFactory).mockReturnValue({
         v1: {
           getApiV1BoxesUnspentByaddressP1: async () => {
-            return {
-              items: [
-                {
-                  assets: [
-                    {
-                      tokenId:
-                        '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                      amount: 10n,
-                    },
-                  ],
-                  additionalRegisters: {
-                    R4: {
-                      serializedValue:
-                        '0e20d4e03eda58a338f8f65b40de258407dbdbbd9b8ccca374f66be8d97e52c8a752',
-                    },
-                  },
-                },
-                {
-                  assets: [
-                    {
-                      tokenId:
-                        '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                      amount: 12n,
-                    },
-                  ],
-                  additionalRegisters: {
-                    R4: {
-                      serializedValue:
-                        '0e20d4e03eda58a338f8f65b40de258407dbdbbd9b8ccca374f66be8d97e52c8a752',
-                    },
-                  },
-                },
-                {
-                  assets: [
-                    {
-                      tokenId:
-                        '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                      amount: 10n,
-                    },
-                  ],
-                  additionalRegisters: {
-                    R4: {
-                      serializedValue:
-                        '0e20c1f5aa4b1a713396a63f556df49c70add93b3471cbca893c6146fce5a4b95e76',
-                    },
-                  },
-                },
-                {
-                  assets: [
-                    {
-                      tokenId:
-                        '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                      amount: 10n,
-                    },
-                  ],
-                  additionalRegisters: {},
-                },
-              ],
-              total: 3,
-            };
+            return explorerUnspentBoxesByAddress;
           },
         },
       } as unknown as ReturnType<typeof ergoExplorerClientFactory>);
--- packages/permit-check/tests/node.spec.ts
+++ packages/permit-check/tests/node.spec.ts
@@ -2,6 +2,7 @@ import ergoNodeClientFactory from '@rosen-clients/ergo-node';
 import { describe, expect, it, vitest } from 'vitest';
 
 import { TestNodePermitHealthCheck } from './testNode';
+import { nodeUnspentBoxesByAddress } from './testData';
 
 vitest.mock('@rosen-clients/ergo-node');
 
@@ -25,57 +26,7 @@ describe('NodePermitAssetHealthCheckParam', () => {
         getBoxesByAddressUnspent: async () => {
           if (firstOut) {
             firstOut = false;
-            return [
-              {
-                assets: [
-                  {
-                    tokenId:
-                      '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                    amount: 10n,
-                  },
-                ],
-                additionalRegisters: {
-                  R4: '0e20d4e03eda58a338f8f65b40de258407dbdbbd9b8ccca374f66be8d97e52c8a752',
-                  R5: '0e0100',
-                },
-              },
-              {
-                assets: [
-                  {
-                    tokenId:
-                      '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                    amount: 12n,
-                  },
-                ],
-                additionalRegisters: {
-                  R4: '0e20d4e03eda58a338f8f65b40de258407dbdbbd9b8ccca374f66be8d97e52c8a752',
-                  R5: '0e0100',
-                },
-              },
-              {
-                assets: [
-                  {
-                    tokenId:
-                      '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                    amount: 10n,
-                  },
-                ],
-                additionalRegisters: {
-                  R4: '0e20c1f5aa4b1a713396a63f556df49c70add93b3471cbca893c6146fce5a4b95e76',
-                  R5: '0e0100',
-                },
-              },
-              {
-                assets: [
-                  {
-                    tokenId:
-                      '383d70ab083cc23336a46370fe730b2c51db0e831586b6d545202cbc33938ee1',
-                    amount: 10n,
-                  },
-                ],
-                additionalRegisters: {},
-              },
-            ];
+            return nodeUnspentBoxesByAddress;
           } else return [];
         },
       } as unknown as ReturnType<typeof ergoNodeClientFactory>);
