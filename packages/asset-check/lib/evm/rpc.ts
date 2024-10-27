96858bbbce52d732a6703b3d57d9b715f4904760
HFazelinia <hadifazelinia78@gmail.com>
2024-10-27 10:57:16 +0000

    change ethereum asset check to evm asset check

--- packages/asset-check/lib/evm/rpc.ts
+++ packages/asset-check/lib/evm/rpc.ts
@@ -2,12 +2,13 @@ import { ethers, JsonRpcProvider } from 'ethers';
 
 import { AbstractAssetHealthCheckParam } from '../abstract';
 import { PartialERC20ABI } from './types';
-import { ETHEREUM_NATIVE_ASSET } from '../constants';
 
-export class EthereumRpcAssetHealthCheckParam extends AbstractAssetHealthCheckParam {
+export class EvmRpcAssetHealthCheckParam extends AbstractAssetHealthCheckParam {
   protected readonly provider: JsonRpcProvider;
+  protected NATIVE_TOKEN_ID: string;
 
   constructor(
+    NATIVE_TOKEN_ID: string,
     assetId: string,
     assetName: string,
     address: string,
@@ -20,12 +21,13 @@ export class EthereumRpcAssetHealthCheckParam extends AbstractAssetHealthCheckPa
   ) {
     super(
       assetId,
-      assetName,
+      assetName === NATIVE_TOKEN_ID ? assetName.toUpperCase() : assetName,
       address,
       warnThreshold,
       criticalThreshold,
       assetDecimal,
     );
+    this.NATIVE_TOKEN_ID = NATIVE_TOKEN_ID;
     this.provider = authToken
       ? new JsonRpcProvider(`${url}/${authToken}`)
       : new JsonRpcProvider(`${url}`);
@@ -38,7 +40,7 @@ export class EthereumRpcAssetHealthCheckParam extends AbstractAssetHealthCheckPa
    * update health status for this param
    */
   updateStatus = async () => {
-    if (this.assetId == ETHEREUM_NATIVE_ASSET) {
+    if (this.assetId == this.NATIVE_TOKEN_ID) {
       this.tokenAmount = await this.provider.getBalance(this.address);
     } else {
       const contract = new ethers.Contract(
