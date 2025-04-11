6689883afae5a7ac658d55fb2d107a48bf9f0adb
mehran <mehran@protonmail.com>
2025-04-11 15:31:59 -0700

    Add asset-check for blockcypher

---
+++ packages/asset-check/lib/doge/blockcypher.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosInstance } from 'axios';
+import { DOGE_NATIVE_ASSET } from '../constants';
+import { AbstractAssetHealthCheckParam } from '../abstract';
+import { BlockCypherAddress } from './types';
+
+export class DogeBlockCypherAssetHealthCheckParam extends AbstractAssetHealthCheckParam {
+  protected client: AxiosInstance;
+
+  constructor(
+    assetName: string,
+    address: string,
+    warnThreshold: bigint,
+    criticalThreshold: bigint,
+    blockCypherUrl: string,
+    assetDecimal = 0,
+  ) {
+    super(
+      DOGE_NATIVE_ASSET,
+      assetName.toUpperCase(),
+      address,
+      warnThreshold,
+      criticalThreshold,
+      assetDecimal,
+    );
+    this.client = axios.create({
+      baseURL: blockCypherUrl,
+    });
+  }
+
+  /**
+   * update health status for this param
+   */
+  updateStatus = async () => {
+    const addressStats = (
+      await this.client.get<BlockCypherAddress>(`/api/address/${this.address}`)
+    ).data;
+    this.tokenAmount = BigInt(addressStats.balance);
+  };
+}
---
+++ packages/asset-check/lib/doge/types.ts
@@ -0,0 +1,25 @@
+export interface BlockCypherTxRef {
+  tx_hash: string;
+  block_height: number;
+  tx_input_n: number;
+  tx_output_n: number;
+  value: number;
+  ref_balance: number;
+  spent: boolean;
+  confirmations: number;
+  confirmed: string;
+  double_spend: boolean;
+}
+
+export interface BlockCypherAddress {
+  address: string;
+  total_received: number;
+  total_sent: number;
+  balance: number;
+  unconfirmed_balance: number;
+  final_balance: number;
+  n_tx: number;
+  unconfirmed_n_tx: number;
+  final_n_tx: number;
+  txrefs: BlockCypherTxRef[];
+}
